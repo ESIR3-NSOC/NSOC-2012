ca006fff86d2e970ef5d25a843cbd5ff8eb2a0e9
vincentledru <vin.ledru@gmail.com>
2012-12-18 17:08:08 +0100

    archi vincent

---
+++ esir3.nsh.projet/src/esir3/nsh/projet/communication/Communication.java
@@ -0,0 +1,16 @@
+package esir3.nsh.projet.communication;
+
+
+import esir3.nsh.projet.communication.Communication;
+
+
+public class Communication {
+
+	Communication com;
+
+
+
+	public Communication (Communication com) {	
+		this.com = com;
+	}
+}
