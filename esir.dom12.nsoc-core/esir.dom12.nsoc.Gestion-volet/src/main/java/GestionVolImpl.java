b454a0d97f3766303ccafb838971ce31957792e9
glefloch <glfloch@gmail.com>
2012-12-25 11:44:59 +0100

    Ajout Log

--- esir.dom12.nsoc-core/esir.dom12.nsoc.Gestion-volet/src/main/java/GestionVolImpl.java
+++ esir.dom12.nsoc-core/esir.dom12.nsoc.Gestion-volet/src/main/java/GestionVolImpl.java
@@ -1,4 +1,6 @@
 import java.net.Inet4Address;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,14 +14,18 @@ public class GestionVolImpl implements GestionVolInterface {
     /**
      * Global Variable
      */
-    String ipadd;
-    Inet4Address ip;
+    String ipadd; // Adresse de la machine
+    Inet4Address ip;// Objet pour connaitre l'adresse ip de notre machine
+    String ipPasserelle; // Adresse ip de la passerelle
+    Logger log = Logger.getLogger(GestionVolImpl.class.getName());
+
 
     /**
      * Constructeur
      */
     public GestionVolImpl() {
         ipadd = ip.getHostAddress();
+        log.log(Level.INFO, "Instance créée");
     }
 
     /**
@@ -37,21 +43,25 @@ public class GestionVolImpl implements GestionVolInterface {
         // Switch case impossible sur une variable String...
         if (type.equals("KNX")) {
 
+            log.log(Level.INFO, "KNX selectionné");
             // Cas ou l'on veut commander des équipements KNX
 
 
         } else if (type.equals("Dali")) {
 
+            log.log(Level.INFO, "Dali selectionné");
             // Cas ou l'on veut commander des equipement Dali
             System.out.println("Les équipements dali ne sont pas encore commandables");
             return false;
         } else if (type.equals("Bacnet")) {
 
+            log.log(Level.INFO, "Bacnet selectionné");
             // Cas ou l'on veut commander des équipement Bacnet
             System.out.println("Les équipements Bacnet ne pas encore commandables");
             return false;
         } else {
 
+            log.log(Level.INFO, "appareil inconnu selectionné");
             // Cas ou le type d'équipement est inconnu
             System.out.println("Equipements non reconnu");
             return false;
