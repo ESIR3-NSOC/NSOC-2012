6abda4be0f5a51a0540b9ed5b6e764debe2d8f1a
antoinethebault <antoine.thebaul@gmail.com>
2013-01-21 08:59:49 +0100

    ade

--- workspace/donneesAde/src/esir/dom12/nsoc/donneesAde/donneesArbre.java
+++ workspace/donneesAde/src/esir/dom12/nsoc/donneesAde/donneesArbre.java
@@ -5,13 +5,18 @@ import java.net.MalformedURLException;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.ScriptException;
-import com.gargoylesoftware.htmlunit.TextPage;
+import com.gargoylesoftware.htmlunit.ScriptResult;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.WebWindow;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlImageInput;
+import com.gargoylesoftware.htmlunit.html.HtmlOption;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
@@ -29,14 +34,15 @@ public class donneesArbre {
 	static void recupererArbre(String requete) throws FailingHttpStatusCodeException, MalformedURLException, IOException, ScriptException{
 
 		// Create a method instance.
-		String url = "http://plannings.univ-rennes1.fr/ade/standard/gui/tree.jsp?calType=ical&login=ical&password=visu&projectId=31&"+requete;
+		String url="http://plannings.univ-rennes1.fr/ade/standard/gui/tree.jsp";
+		//String url="http://plannings.univ-rennes1.fr/ade/standard/gui/interface.jsp";
+		//String url = "http://plannings.univ-rennes1.fr/ade/standard/gui/tree.jsp?calType=ical&login=ical&password=visu&projectId=31&"+requete;
 		//String url="http://plannings.univ-rennes1.fr/ade/custom/modules/plannings/direct_cal.jsp?calType=ical&login=cal&password=visu&resources=349&firstDate=2012-12-19&lastDate=2012-12-19&projectId=31";
 		System.out.println("url : "+url+"\n-----------------\n");
     
 		final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_10);
+		WebWindow ww = null;
 	    HtmlPage page = webClient.getPage(url);
-	    String rep = page.getWebResponse().getContentAsString();
-	    System.out.println(rep);
 	    
 	    // Get the form that we are dealing with and within that form, 
 	    // find the submit button and the field that we want to change.
@@ -44,17 +50,24 @@ public class donneesArbre {
 	    final HtmlSubmitInput button = form.getInputByName("submit");
 	    final HtmlTextInput usernameTextField = form.getInputByName("username");
 	    final HtmlPasswordInput passwordTextField = form.getInputByName("password");
-
 	    // Change the value of the text field
 	    usernameTextField.setValueAttribute("29003625");
-	    passwordTextField.setValueAttribute("");
-
+	    passwordTextField.setValueAttribute("kangblo!");
 	    // Now submit the form by clicking the button and get back the second page.
-	    HtmlPage page2 = button.click();
-	    
-	    rep = page2.getWebResponse().getContentAsString();
-	    System.out.println("---------------------------------------------------------------------------------------------------------\n\n\n"+rep);
-	    
+	    HtmlPage page2 = button.click();	    
+	   String rep = page2.getWebResponse().getContentAsString();
+	    System.out.println(rep+"-------------------------51--------------------------------------------------------------------------------\n\n\n");
+
+	    /*HtmlSelect select = (HtmlSelect) page2.getElementByName("projectId");
+	    HtmlOption option = select.getOptionByText("BEAULIEU 2012-2013");
+	    select.setSelectedAttribute(option, true);*/
+	    ww.setEnclosedPage(page2);
+	    final HtmlImageInput ip = page2.getFirstByXPath("//input");//[@nsrc='/ade/button?text=Ok&red=false&cssClass=okbutton']
+	    HtmlPage page3 = (HtmlPage) ip.click();
+	    rep = page3.getWebResponse().getContentAsString();
+	    System.out.println(rep+"-------------------------------62--------------------------------------------------------------------------\n\n\n");
+	    ScriptResult sr = page3.executeJavaScript("checkCategory('trainee')");
+	    System.out.println("script result... :  "+sr.toString());
 	    webClient.closeAllWindows();
 	}
 }
---
+++ workspace/donneesAde/src/esir/dom12/nsoc/donneesAde/fonctionsAde.java
@@ -0,0 +1,8 @@
+package esir.dom12.nsoc.donneesAde;
+
+public class fonctionsAde {
+	public String planningDeuxJours (String salle, String date){
+		
+		return null;
+	}
+}
