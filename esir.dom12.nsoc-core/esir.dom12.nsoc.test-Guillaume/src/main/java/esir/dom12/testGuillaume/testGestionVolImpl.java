cb3368e9daadaae25d600b21fb38d6f12d268434
glefloch <glfloch@gmail.com>
2013-01-30 17:43:06 +0100

    cleanning

---
+++ esir.dom12.nsoc-core/esir.dom12.nsoc.test-Guillaume/src/main/java/esir/dom12/testGuillaume/testGestionVolImpl.java
@@ -0,0 +1,11 @@
+package esir.dom12.testGuillaume;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guillaumelefloch
+ * Date: 1/30/13
+ * Time: 2:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class testGestionVolImpl {
+}
