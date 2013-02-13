e4020181e6f98d5c8584b8d1d9778088dc974986
marc.duval <marc.dvl35@gmail.com>
2013-02-13 11:18:05 +0100

    Conversion NFC to nom, prenom Etudiant

--- esir.dom12.nsoc.NFCTranslator/target/generated-sources/kevoree/esir/dom12/nsoc/nfc/kevgen/JavaSENode/NFCTranslatorPORTentreeFromGaToNfc.scala
+++ esir.dom12.nsoc.NFCTranslator/target/generated-sources/kevoree/esir/dom12/nsoc/nfc/kevgen/JavaSENode/NFCTranslatorPORTentreeFromGaToNfc.scala
@@ -12,7 +12,7 @@ msgcall.getParams.put("numeroTagNfc",numeroTagNfc.asInstanceOf[AnyRef])
 (this !? msgcall).asInstanceOf[java.lang.String]}
 override def internal_process(msg : Any)= msg match {
 case opcall : org.kevoree.framework.MethodCallMessage => reply(opcall.getMethodName match {
-case "sendNumeroTagNFCFromGestionAccesToNfc"=> try { component.receiveNumeroTagNFCFromGestionAcces(if(opcall.getParams.containsKey("numeroTagNfc")){opcall.getParams.get("numeroTagNfc").asInstanceOf[java.lang.String]}else{opcall.getParams.get("arg0").asInstanceOf[java.lang.String]})} catch {case _ @ e => e.printStackTrace();println("Uncatched exception while processing Kevoree message");null }
+case "sendNumeroTagNFCFromGestionAccesToNfc"=> try { component.sendNumeroTagNFCFromGestionAccesToNfc(if(opcall.getParams.containsKey("numeroTagNfc")){opcall.getParams.get("numeroTagNfc").asInstanceOf[java.lang.String]}else{opcall.getParams.get("arg0").asInstanceOf[java.lang.String]})} catch {case _ @ e => e.printStackTrace();println("Uncatched exception while processing Kevoree message");null }
 case _ @ o => println("uncatch message , method not found in service declaration : "+o);null 
 })}
 }
